8f2c3ee7dd87108ceb99d0a76cf6bf0634532dc3
dma-cmyk <marotya@protonmail.com>
2023-09-09 19:20:26 +0900

    Update wired.sh

--- wired.sh
+++ wired.sh
@@ -2,6 +2,19 @@
 
 read -rp "Android端末をUSBで接続して下さい"
 
+echo ""
+
+read -rp "使用するポートを入力して下さい：" Port
+adb tcpip "$Port"
+
+echo ""
+
+read -rp "Android端末のUSB切断して下さい"
+
+echo ""
+
+read -rp "接続する端末のIPを入力して下さい:" IP
+adb connect "$IP:$Port"
 
 cat << 'EOF'
 
@@ -17,7 +30,6 @@ cat << 'EOF'
 
 EOF
 
-
-read -rp "デバイスがあったらEnter "
+read -rp "↑デバイスが認識できていればミラーリングを開始できます"
 
 scrcpy
